f50e6a75af05c144b5239555f491d69035fdd728
Ninnjah <45268461+Ninnjah@users.noreply.github.com>
2020-06-24 12:50:48 +0300

    Update Morse.py

--- Morse/MorseCMD/Morse.py
+++ Morse/MorseCMD/Morse.py
@@ -1,26 +1,30 @@
 from tkinter import Tk
 c = Tk()
+#Decoration // Декорация
 print('-.. . -.-. --- .-. .- - .. --- -.\n..-. Translater to morse .-..\n-.. . -.-. --- .-. .- - .. --- -.')
+#Dictionaries // Словари
 dictA = {'a':'.- ' ,'b':'-... ','c':'-.-. ','d':'-.. ','e':'. ','f':'..-. ','g':'--. ','h':'.... ','i':'.. ','j':'.--- ','k':'-.- ','l':'.-.. ','m':'-- ','n':'-. ','o':'--- ','p':'.--. ','q':'--.- ','r':'.-. ','s':'... ','t':'- ','u':'..- ','v':'...- ','w':'.-- ','x':'-..- ','y':'-.-- ','z':'--.. ','0':'----- ','9':'----. ','8':'---.. ','7':'--... ','6':'-.... ','5':'..... ','4':'....- ','3':'...-- ','2':'..--- ','1':'.---- ',' ':'_ '}
 dictB = {'.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f','--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l','--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r','...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x','-.--':'y','--..':'z','-----':'0','----.':'9','---..':'8','--...':'7','-....':'6','.....':'5','....-':'4','...--':'3','..---':'2','.----':'1','_':' '}
 
+#Main cycle // Главный цикл
 while True:
-    cmdRaw=input('TO morse or FROM morse?  to/from: ')
+    cmdRaw=input('TO morse or FROM morse?  to/from: ') #Request command // Запрос команды
     cmd = cmdRaw.lower()
-    if cmd == 'to':
+    if cmd == 'to': #Encrypting to morse // Шифровка в морзе
         morseA = input('Enter phrase to morse: ')
         morse = morseA.lower()
         morseB = ''
         for i in morse:
             morseB += dictA[i]
         print('Morse: ', morseB)
+        #Copy to buffer // Копирование в буфер
         c.withdraw()
         c.clipboard_clear()
         c.clipboard_append(morseB)
         c.update
         c.destroy
         
-    elif cmd == 'from':
+    elif cmd == 'from': #Decrypting from morse // Расшифровка из морзе
         cmdRaw=input('Paste copied morse? y/n: ')
         morseB = ''
         cmd = cmdRaw.lower()
